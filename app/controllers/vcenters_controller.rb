ed5e4f3f8803ab228e61e0cc0f1a7d7c926c5938
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-19 19:10:41 -0400

    t

--- app/controllers/vcenters_controller.rb
+++ app/controllers/vcenters_controller.rb
@@ -5,36 +5,10 @@ module ForemanVcenterDemo
       def index
         @vcenters = resource_base_search_and_page
       end
+      
       def new
         @vcenter = Vcenter.new
       end
-  
-      def create
-        @vcenter = Vcenter.new(vcenter_params)
-        if @vcenter.save
-          redirect_to vcenters_path, notice: 'Vcenter was successfully created.'
-        else
-          render :new
-        end
-      end
-  
-      def edit
-        @vcenter = Vcenter.find(params[:id])
-      end
-  
-      def update
-        @vcenter = Vcenter.find(params[:id])
-        if @vcenter.update(vcenter_params)
-          redirect_to vcenters_path, notice: 'Vcenter was successfully updated.'
-        else
-          render :edit
-        end
-      end
-  
-      private
-  
-      def vcenter_params
-        params.require(:vcenter).permit(:name, :fqdn, :enclave)
-      end
+
     end
   end
