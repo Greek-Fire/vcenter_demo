a32815c4e81c70eba1039413dde9bd2adcc1e75e
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-20 08:59:22 -0400

    t

--- app/models/vcenter.rb
+++ app/models/vcenter.rb
@@ -2,10 +2,10 @@ class Vcenter < ApplicationRecord
   include Authorizable
   include Taxonomix
 
-    validates :name, :fqdn, :enclave, presence: true
-    scoped_search on: :name, complete_value: true
-    scoped_search on: :fqdn, complete_value: true
-    scoped_search on: :enclave, complete_value: true
+  validates :name, :fqdn, :enclave, presence: true
+  scoped_search on: :name, complete_value: true
+  scoped_search on: :fqdn, complete_value: true
+  scoped_search on: :enclave, complete_value: true
     
 end
   
