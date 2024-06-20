03639bf3131d509aaa74108f7a41794671b3ab95
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-19 21:05:32 -0400

    t

--- app/controllers/vcenters_controller.rb
+++ app/controllers/vcenters_controller.rb
@@ -1,14 +1,29 @@
 module ForemanVcenterDemo
-    class VcentersController < ApplicationController
+  class VcentersController < ApplicationController
       include Foreman::Controller::AutoCompleteSearch
       
       def index
         @vcenters = resource_base_search_and_page
       end
-      
+
       def new
         @vcenter = Vcenter.new
       end
 
-    end
+      def update
+        if @build_report.update(build_report_params)
+          process_success
+        else
+          process_error
+        end
+      end
+    
+      def destroy
+        if @build_report.destroy
+          process_success
+        else
+          process_error
+        end
+      end
   end
+end
