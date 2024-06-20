9b32772367590297c95fa8f41d9a0b8f826ecf00
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-20 11:45:43 -0400

    t

--- app/models/vcenter.rb
+++ app/models/vcenter.rb
@@ -2,16 +2,9 @@ class Vcenter < ApplicationRecord
   include Authorizable
   include Taxonomix
 
-  validates :name, :fqdn, :enclave, presence: true
+  #validates :name, :fqdn, :enclave, presence: true
   scoped_search on: :name, complete_value: true
   scoped_search on: :fqdn, complete_value: true
   scoped_search on: :enclave, complete_value: true
   
-  def self.fetch(params)
-    list = self
-      .search_for(params[:search])
-      .order(params[:order])
-      .paginate(page: params[:page], per_page: params[:per_page])
-    list
-  end
 end
