395bcc2b9f1c8ea674ac0c13895ab4bd279f7f57
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-20 11:26:03 -0400

    t

--- app/controllers/vcenters_controller.rb
+++ app/controllers/vcenters_controller.rb
@@ -6,7 +6,7 @@ module ForemanVcenterDemo
     before_action :find_vcenter, only: [:edit, :update, :destroy, :show]
 
     def index
-      @vcenters = Vcenter.fetch(vcenter_params)
+      @vcenters = resource_base_search_and_page
     end
 
     def new
