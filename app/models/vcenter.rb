9cbafb3f6b06169f3eea00f5c8c5a8ca72e73b1f
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-20 09:42:36 -0400

    t

--- app/models/vcenter.rb
+++ app/models/vcenter.rb
@@ -6,6 +6,15 @@ class Vcenter < ApplicationRecord
   scoped_search on: :name, complete_value: true
   scoped_search on: :fqdn, complete_value: true
   scoped_search on: :enclave, complete_value: true
-    
+  
+  def self.fetch(params)
+    list = self
+      .with_counts
+      .search_for(params[:search])
+      .order(params[:order])
+      .paginate(page: params[:page], per_page: params[:per_page])
+    list
+  end
+  
 end
   
