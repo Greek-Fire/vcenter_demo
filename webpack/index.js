0f5d7d4216b77f59f6f3da1383f111fd1fb6445a
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-20 10:37:51 -0400

    t

--- webpack/index.js
+++ webpack/index.js
@@ -1,1 +1,5 @@
 import componentRegistry from 'foremanReact/components/componentRegistry';
+import EmptyState from 'foremanReact/components/EmptyState';
+
+// Register the component for use
+componentRegistry.register({ name: 'EmptyState', type: EmptyState });
