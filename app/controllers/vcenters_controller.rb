353c4a4c6f165a9726746c5544209ce13f8d157c
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-20 09:33:07 -0400

    t

--- app/controllers/vcenters_controller.rb
+++ app/controllers/vcenters_controller.rb
@@ -4,7 +4,7 @@ module ForemanVcenterDemo
       include Foreman::Controller::AutoCompleteSearch
       
       def index
-        @vcenters = resource_base_search_and_page
+        @vcenters = resource_base_search_and_page.search_for(params[:search])
       end
 
       def new
